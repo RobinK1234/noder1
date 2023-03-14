fe7d66712552b3a0858dd35250ce1daffeb65ab3
Robin <robin.kriisa@voco.ee>
2023-03-14 10:40:24 +0200

    productlist

--- veebirakendus/index.ts
+++ veebirakendus/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import stringsController from "./controllers/strings";
+import productsController from "./controllers/products";
+import productlistController from "./controllers/productlist";
 
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
+
+app.use('/', productlistController);
+app.use('/', stringsController);
+app.use('/', productsController)
 
 app.listen(3000,() => {
     console.log(`[server]: Server is running at http://localhost:3000`);
